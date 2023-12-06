e2230408e49dc674fda35998d5f383da4410eb45
ISLAM MD TARCCQUL <tareq2059@gmail.com>
2023-12-06 00:00:17 +0000

    Update table field (step 1)

--- database/migrations/2023_12_05_140005_create_leave_types_table.php
+++ database/migrations/2023_12_05_140005_create_leave_types_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\LeaveType;
+
 return new class extends Migration
 {
     /**
@@ -13,10 +15,18 @@ return new class extends Migration
     {
         Schema::create('leave_types', function (Blueprint $table) {
             $table->id();
+            $table->string('leavetype_code')->unique();
+            $table->integer('leavetype_block')->nullable();
+            $table->string('leavetype_desc')->nullable();
+            $table->integer('leavetype_default')->nullable();
+            $table->integer('leavetype_bringover')->nullable();
+            $table->integer('leavetype_custom_field')->nullable();
+            $table->integer('industry_seqno')->nullable();
+            $table->integer('industry_status')->default(1);
             $table->timestamps();
         });
     }
-
+    //LeaveType::create(['leavetype_code' => 'Annual Leave','email' => 'admin@themesbrand.com','password' => Hash::make('12345678'),'role_id'=>'1','parent_id'=>'0', 'email_verified_at'=> now(), 'created_at' => now()]); 
     /**
      * Reverse the migrations.
      */
