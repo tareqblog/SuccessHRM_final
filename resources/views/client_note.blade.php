957430cda87ac8360b4377cf9a42c171552fd136
ISLAM MD TARCCQUL <tareq2059@gmail.com>
2023-12-11 00:53:46 +0000

    update client database
    add client note for work

---
+++ resources/views/client_note.blade.php
@@ -0,0 +1,69 @@
+.	Report Generation - Refer to list below
+2.	Personal Folder Search - enable within resume/remarks keyword search
+3.	Saving resumes from job applicants –
+a.	With Job Ad - Applicants from job applicants, does not auto assign to job ad owner
+b.	With NO Job Ad (Just applying randomly with no specific position) – AUTO assign to JB (Manager)
+c.	New applicant:  
+i.	Once generated, even though candidate uploaded resume through the website, resume does not auto load or is not consistently being saved.
+ii.	No right click functionality / does not open a new window to generate resulting to more clicks and waiting instead of the same page staying on the same view.
+d.	Has existing entry in ATS:  
+i.	ATS allows to generate again (inconsistent) resulting to 2 entries
+ii.	ATS acknowledges that candidates are repeat, and when the candidate uploads their NEW Most Updated resume again, ATS does not auto save in the “candidate search” tab and we have to manually download then manually upload to update the resume.
+4.	Create “Shortlisted’ Folder in the dashboard
+5.	Improve search functionality in ALL the folders on the dashboard including remarks in ALL folders and position sent, client name sent to in the Total Candidates Assigned to Clients.
+6.	Multi-Batch uploading improvement – 
+a.	if 1 file is corrupt, all else should be able to upload compared to now where all cannot be uploaded and have to do 1 by 1 in order to know which file is corrupt.
+b.	5 or more resumes upload in one batch, chance of resume being NOT viewable is very high 
+7.	Resume UNVIEWABLE going to Success Website or some error message OR sometimes need to repeatedly click VIEW before loading
+8.	Enable “Delete Import File” in TL and Intern Accounts too compared to now which is only available in the Master and Manager account.  
+9.	Enable re-assigning to another colleague or other team member BUT SHOULD STILL ENSURE that ONLY their TL/Manager can still view dashboard
+10.	Enable BATCH Reassign
+11.	Must click “add remarks” before can see the remarks instead of just remarks - 80440902 / 82678901
+12.	Disable editing previous remarks to ALL except MASTER/MANAGER
+13.	Remove “New Candidates assigned to you” folder on the dashboard
+14.	Add “Rework” folder on the dashboard – resumes that were previously assigned to the same RC and needs rework
+15.	Ensure no duplicate candidate exactly the same - 83586773
+NEW (NOT DISCUSSED YET with IT):
+
+1.	Call Back
+a.	Create “Call Back” commitment folder on the dashboard
+b.	ALL Call Back should show in the calendar too with date and time
+c.	Call Backs within 5 Business Days - should have an option to put call back appointments (Candidate should still reflect in the Day 1-5 follow up folder PLUS also in the separate “Call Back” commitment folder on the dashboard for easier viewing
+d.	For Call Backs more than 5 Business Days - should NOT reflect in the D1-D5 follow up folder follow up BUT instead should reflect in the KIV folder PLUS also in the separate “Call Back” commitment folder on the dashboard for easier viewing
+2.	Create “3 Business Days NO Action” folder – Auto transfer no actioned resumes after 3 business days
+3.	Add the following in “Remark Type”
+a.	FAJ
+b.	Not Suitable (all candidates should be deemed closed case if put under not suitable even after interview with client to ensure no pending cases in ATS and remarks should be added by RC)
+c.	KIV
+d.	Blacklist
+4.	Enable “Cancel” in the START DATE if candidate is shortlisted (Do like the one for interview cancellation)
+5.	$$ Sales (ATS has option to key in Salary once shortlisted but not sales division yet – should be editable)
+6.	Create “Follow Up with Client” Folder with 2 Sub-Folders like (Follow Ups)
+a.	“2 Business Days Follow Up with Client” - After assigning to “Total Candidates Assigned to Client”, AUTO transfer to “2 Business Days Follow Up with Client” Folder in 2 business days and SHOW IN CALENDAR @ 9am and SHOW in the CALENDAR too
+b.	“Post Interview Follow Up with Client” - After scheduling/assigning under “Interview”, AUTO transfer to “Post Interview Follow Up with Client” the next day @ 9am and SHOW in the CALENDAR too
+REPORT GENERATION:
+
+1.	Option to generate - 
+a.	per individual/Team/all under Manager Account
+b.	for Daily/Weekly/Monthly/Quarterly/Annually/customised
+c.	with graphs if possible but NOT a must
+2.	Report - with TOTAL for each
+a.	NEW ACTIVE Folder 
+b.	FOLLOW UP Folder
+i.	Day 1
+ii.	Day 2
+iii.	Day 3
+iv.	Day 4
+v.	Day 5
+vi.	Call Back Commitment 
+c.	Follow Up with Client Folder
+i.	2 Business Days Follow Up with Client
+ii.	Post Interview Follow Up with Client
+d.	Interview Folder
+e.	Not Suitable / FAJ / Blacklist Folder
+f.	KIV
+g.	Total Candidates Assigned to Client
+h.	Shortlisted Folder
+i.	3 Business Days NOT ACTIONED resumes
+j.	Rework Folder
+k.	$$ Sales
