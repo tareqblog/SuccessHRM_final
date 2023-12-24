d00c7643ee84cf16eb26a7763861a982d432a1e9
ISLAM MD TARCCQUL <tareq2059@gmail.com>
2023-12-24 01:08:54 +0000

    Squashed commit of the following:
    commit 918b9862458f38515abb74a9688379562c20e7e0
    Author: Md. Perves Hossen <mdperves127@gmail.com>
    Date:   Sat Dec 23 20:57:16 2023 +0600
    
        candidate file change type on/off done
    
    commit b0b00469154281b475efb42bbc09376026c3482f
    Author: Md. Perves Hossen <mdperves127@gmail.com>
    Date:   Fri Dec 22 12:37:33 2023 +0600
    
        candidate left work done
    
    commit 1624786ca9f701331c99cde28f3d2eca4ebe8171
    Author: Md. Perves Hossen <mdperves127@gmail.com>
    Date:   Thu Dec 21 22:02:20 2023 +0600
    
        candidate remark & candidate payroll done

---
+++ database/migrations/2023_12_23_081756_create_nationalities_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nationalities', function (Blueprint $table) {
+            $table->id();
+            $table->string('nationality_code');
+            $table->string('nationality_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nationalities');
+    }
+};
--- resources/views/admin/candidate/index.blade.php
+++ resources/views/admin/candidate/index.blade.php
@@ -1,89 +1,89 @@
 @extends('layouts.master')
 @section('title')
-Candidate Management
+    Candidate Management
 @endsection
 @section('page-title')
-Candidate Management
+    Candidate Management
 @endsection
 @section('css')
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
 @endsection
 @section('body')
 
-<body>
+    <body>
     @endsection
     @section('content')
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title mb-0">Candidate Table</h4>
-                    <div class="text-end">
-                        <a href="{{route('candidate.create')}}" class="btn btn-sm btn-success">Create New</a>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title mb-0">Candidate Table</h4>
+                        <div class="text-end">
+                            <a href="{{ route('candidate.create') }}" class="btn btn-sm btn-success">Create New</a>
+                        </div>
                     </div>
-                </div>
-                <div class="card-body">
-                    <table class="table table-bordered mb-0" id="myTable">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>ID</th>
-                                <th>NRIC</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Mobile</th>
-                                <th>Last Update Date</th>
-                                <th>Job Assign</th>
-                                <th>Group</th>
-                                <th>Status</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($datas as $data)
+                    <div class="card-body">
+                        <table class="table table-bordered mb-0" id="myTable">
+                            <thead>
                                 <tr>
-                                    <td>
-                                        {{ $loop->index + 1 }}
-                                    </td>
-                                    <td>{{ $data->id}}</td>
-                                    <td>{{ $data->candidate_nric }}</td>
-                                    <td>{{ $data->candidate_name }}</td>
-                                    <td>{{ $data->candidate_email }}</td>
-                                    <td>{{ $data->candidate_mobile }}</td>
-                                    <td>{{ $data->updated_at }}</td>
-                                    <td><a href="{{ route('candidate.edit', $data->id) }}"
-                                            class="btn btn-success btn-sm me-3">Assigned</a></td>
-                                    <td></td>
-                                    <td>{{ $data->candidate_status == 1 ? 'Active' : 'In-Active' }}</td>
-                                    <td style="display: flex;">
-                                        <a href="{{ route('candidate.edit', $data->id) }}#remark"
-                                            class="btn btn-warning btn-sm me-3"></i>Remarks</a>
-                                        <a href="{{ route('candidate.edit', $data->id) }}"
-                                            class="btn btn-info btn-sm me-3"><i class="fas fa-pen"></i></a>
-                                        <form id="deleteForm" action="{{ route('candidate.destroy', $data->id) }}"
-                                            method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit"
-                                                onclick="return confirm('Are you sure you want to delete this item?')"
-                                                class="btn btn-sm btn-danger"> <i class="fas fa fa-trash"></i> </a>
-                                        </form>
-                                    </td>
+                                    <th>No</th>
+                                    <th>ID</th>
+                                    <th>NRIC</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Mobile</th>
+                                    <th>Last Update Date</th>
+                                    <th>Job Assign</th>
+                                    <th>Group</th>
+                                    <th>Status</th>
+                                    <th></th>
                                 </tr>
-                            @empty
+                            </thead>
+                            <tbody>
+                                @forelse ($datas as $data)
+                                    <tr>
+                                        <td>
+                                            {{ $loop->index + 1 }}
+                                        </td>
+                                        <td>{{ $data->id }}</td>
+                                        <td>{{ $data->candidate_nric }}</td>
+                                        <td>{{ $data->candidate_name }}</td>
+                                        <td>{{ $data->candidate_email }}</td>
+                                        <td>{{ $data->candidate_mobile }}</td>
+                                        <td>{{ $data->updated_at }}</td>
+                                        <td><a href="{{ route('candidate.edit', $data->id) }}"
+                                                class="btn btn-success btn-sm me-3">Assigned</a></td>
+                                        <td></td>
+                                        <td>{{ $data->candidate_status == 1 ? 'Active' : 'In-Active' }}</td>
+                                        <td style="display: flex;">
+                                            <a href="{{ route('candidate.edit', $data->id) }}#remark"
+                                                class="btn btn-warning btn-sm me-3"></i>Remarks</a>
+                                            <a href="{{ route('candidate.edit', $data->id) }}"
+                                                class="btn btn-info btn-sm me-3"><i class="fas fa-pen"></i></a>
+                                            <form id="deleteForm" action="{{ route('candidate.destroy', $data->id) }}"
+                                                method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit"
+                                                    onclick="return confirm('Are you sure you want to delete this item?')"
+                                                    class="btn btn-sm btn-danger"> <i class="fas fa fa-trash"></i> </a>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
 
-                                <tr>
-                                    <td colspan="50" class="text-center text-warning">
-                                        No Data found!
-                                    </td>
-                                </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
+                                    <tr>
+                                        <td colspan="50" class="text-center text-warning">
+                                            No Data found!
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     @endsection
 
 
