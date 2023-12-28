54556b44f0cbb2069caa981c6a65bef9f63ee612
ISLAM MD TARCCQUL <tareq2059@gmail.com>
2023-12-28 07:46:03 +0000

    update required file upload

--- app/Http/Controllers/CandidateFileImportController.php
+++ app/Http/Controllers/CandidateFileImportController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\FileHelper;
 use App\Models\CandidateFileImport;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Storage;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Spatie\PdfToText\Pdf;
 class CandidateFileImportController extends Controller
 {
     /**
@@ -61,5 +64,100 @@ class CandidateFileImportController extends Controller
     public function destroy(CandidateFileImport $candidateFileImport)
     {
         //
+    }
+
+    public function upload_old(Request $request)
+    {
+       
+        if ($request->hasFile('files')) {
+            $files = $request->file('files');
+            $uploadedFiles = [];
+
+            foreach ($files as $file) {
+                $extension = $file->getClientOriginalExtension();
+
+                // Check if the file extension is allowed
+                if (in_array($extension, ['pdf', 'doc', 'docx', 'xls', 'xlsx'])) {
+                    // Store the file
+                    $path = FileHelper::uploadFile($file);
+                    // Read the file content (you might need specific logic per file type)
+                    $data = $this->extractData($path, $extension);
+                   
+                    $uploadedFiles[] = [
+                        'path' => $path,
+                        'data' => $data,
+                    ];
+                }
+            }
+
+            return view('admin.candidate.import')->with('uploadedFiles', $uploadedFiles);
+        }
+
+        return back()->with('error', 'No valid files were selected.');
+    }
+    public function upload(Request $request)
+    {
+        $validatedData = $request->validate([
+            'files.*' => 'mimes:pdf,doc,docx,xls,xlsx'
+        ]);
+    
+        if ($request->hasFile('files')) {
+            $uploadedFiles = [];
+            foreach ($request->file('files') as $file) {
+                $filename = $file->getClientOriginalName();
+                $path=FileHelper::uploadFile($file);
+                //    dd($path);
+                //$file->storeAs('uploads', $filename); // Store file in storage/uploads directory
+                $uploadedFiles[] = $path;
+                
+            }
+    
+            return view('admin.candidate.import')->with('uploadedFiles', $uploadedFiles);
+        }
+    
+        return back()->with('error', 'No files were selected.');
+    }
+    public function extractInfo(Request $request)
+    {
+        if ($request->has('selectedFiles')) {
+            $selectedFiles = $request->selectedFiles;
+    
+            foreach ($selectedFiles as $file) {
+                // Process each file to extract information
+                $filePath = public_path().Storage::url($file);
+                //dd($filePath);
+                if (pathinfo($file, PATHINFO_EXTENSION) === 'docx') {
+                    $phpWord = IOFactory::load($filePath);
+                    $docInfo = $phpWord->getDocInfo();
+    
+                    // Extract relevant info from the document
+                    $title = $docInfo->getTitle();
+                    $subject = $docInfo->getSubject();
+                    // Extract email, name, phone, etc.
+                    // Extract the information and pass it to the view
+                } elseif (pathinfo($file, PATHINFO_EXTENSION) === 'pdf') {
+                    // Extract information from PDF files
+                    // Use libraries like TCPDF, FPDI, or others to read PDF contents
+                    //$extractedInfo= (new Pdf())->getText($filePath);
+                    try {
+                        $text = Pdf::getText($filePath);
+                        return response($text, 200);
+                    } catch (\Exception $e) {
+                        return response('Error reading PDF: ' . $e->getMessage(), 500);
+                    }
+                } elseif (pathinfo($file, PATHINFO_EXTENSION) === 'xls || xlsx') {
+                    // Extract information from PDF files
+                    // Use libraries like TCPDF, FPDI, or others to read PDF contents
+                } elseif (pathinfo($file, PATHINFO_EXTENSION) === 'doc') {
+                    // Extract information from PDF files
+                    // Use libraries like TCPDF, FPDI, or others to read PDF contents
+                }
+                // Extract information from other file types similarly (xls, xlsx, doc)
+            }
+            
+            return view('admin.candidate.import')->with('info', $extractedInfo);
+        }
+    
+        return back()->with('error', 'No files were selected.');
     }
 }
